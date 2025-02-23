12f49fc740fe735d823433b06fe2a4015c143c5d
piolorenzoperez <ptperez2@up.edu.ph>
2025-02-23 15:33:13 +0800

    init

--- OneDiliman_Testbench/tests/SprintTwo_Post.test.tsx
+++ OneDiliman_Testbench/tests/SprintTwo_Post.test.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render, screen, waitFor} from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import userEvent from "@testing-library/user-event";
+//import FeedPage from '../src/pages/Feed/FeedPage.tsx'
+//import CreatePostPage from '../src/pages/CreatePost/CreatePostPage.tsx'
 
-test('create a post', () => {
+test('render createpostpage', () => {
+    render(
+        <MemoryRouter><FeedPage /></MemoryRouter>
+    )
+
+    const instructions = screen.getByText(/test/i);
+    expect(instructions).toBeInTheDocument();
+})
+
+test('create and upload a post', () => {
     const postId 
     const postOwner
     const postTitle
