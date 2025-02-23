8c189541144b239eda8242b239ca31cf891725bf
piolorenzoperez <ptperez2@up.edu.ph>
2025-02-23 15:10:02 +0800

    created blank files for feed page, and post testing sprint 2

---
+++ OneDiliman_Testbench/tests/SprintTwo_Post.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen, waitFor} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+test('create a post', () => {
+    const postId 
+    const postOwner
+    const postTitle
+    const postContent
+    const postPictures
+    const postTags
+    const postDate
+    const postTime
+})
