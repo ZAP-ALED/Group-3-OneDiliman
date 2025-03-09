278c9aa36bf244831bcbf6012b4fa1cc27c5727b
piolorenzoperez <ptperez2@up.edu.ph>
2025-03-09 08:47:14 +0800

    init creation

---
+++ OneDiliman_Testbench/tests/SprintThree.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen, waitFor, user, cleanup, fireEvent } from '@testing-library/react';
+import LogInPage from '../src/pages/LogIn/LoginPage.tsx';
+import OrgPage from '../src/pages/OrgPage/OrgPage.tsx';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import DashboardPage from '../src/pages/Dashboard/DashboardPage.tsx';
+
+// testing routed pages credit from: https://stackoverflow.com/questions/76081552/typeerror-cannot-destructure-property-basename-of-react-namespace-usecontex
+
+async function logInOrg() {
+  cleanup();
+  await render(
+    <MemoryRouter><LogInPage /></MemoryRouter>);
+  const user = userEvent.setup();
+
+  const username = screen.getByPlaceholderText("Email");
+  const password = screen.getByPlaceholderText("Password");
+
+  await user.type(username, "org1@up.edu.ph");
+  await user.type(password, "12345678");
+
+  await user.click(screen.getByText(/Log In/));
+}
+
+async function logInUser() {
+  cleanup();
+  await render(
+    <MemoryRouter><LogInPage /></MemoryRouter>);
+  const user = userEvent.setup();
+
+  const username = screen.getByPlaceholderText("Email");
+  const password = screen.getByPlaceholderText("Password");
+
+  await user.type(username, "ptperez2@up.edu.ph");
+  await user.type(password, "useremailtest123");
+
+  await user.click(screen.getByText(/Log In/));
+}
+
+async function logOut() {
+  const user = userEvent.setup();
+  const dropdown = screen.queryAllByTestId("profile-dropdown"); //look into this, why many profile dropdowns?
+  await user.click(dropdown[0]);
+  await user.click(screen.getByTestId("logout-button"));
+}
