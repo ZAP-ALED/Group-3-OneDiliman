4d2134b3764ac27bceda4925b1b819389c6515eb
piolorenzoperez <ptperez2@up.edu.ph>
2025-04-09 05:39:01 +0800

    update

--- OneDiliman_Testbench/tests/SprintTwo.test.tsx
+++ OneDiliman_Testbench/tests/SprintTwo.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, user, cleanup, fireEvent } from '@testing-library/react';
+import { render, screen, waitFor, cleanup, fireEvent } from '@testing-library/react';
 import LogInPage from '../src/pages/LogIn/LoginPage.tsx';
 import OrgPage from '../src/pages/OrgPage/OrgPage.tsx';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
@@ -47,127 +47,136 @@ async function logOut() {
   await user.click(screen.getByTestId("logout-button"));
 }
 
-test('navigate to org page from dashboard, create a post, delete a post', async  () => {
+async function timeout(time: number) {
+  await new Promise((r) => setTimeout(r, time));
+}
+
+async function renderOrgPage() {
+  await render(
+    <MemoryRouter><DashboardPage /></MemoryRouter>);
+  await new Promise((r) => setTimeout(r, 7000));
+
+  const orgCard = await screen.findByTestId("org-card-jO8BwsPe1lSCAo1gIRa6oR8vGpH3");
+  await expect(orgCard).toBeDefined();
+
+  await userEvent.click(orgCard);
+
+  await render(
+      <MemoryRouter initialEntries={['/dashboard/jO8BwsPe1lSCAo1gIRa6oR8vGpH3']}>
+          <Routes>
+              <Route path="/dashboard/:orgId" element ={<OrgPage />} />
+          </Routes>
+      </MemoryRouter>
+  );
+  await new Promise((r) => setTimeout(r, 5000));
+}
+
+test('navigate to org page from dashboard, create a post, edit a post, delete a post', async  () => {
   const spyConfirm = vi.spyOn(window, 'confirm').mockImplementation(() => true);
   const spyAlert = vi.spyOn(window, 'alert').mockImplementation(() => {});
   await logInOrg();
-  cleanup();
+  await cleanup();
 
-  await render(
-    <MemoryRouter><DashboardPage /></MemoryRouter>);
-  const user = userEvent.setup();
-  await new Promise((r) => setTimeout(r, 2000));
-
-  const orgCard = await screen.findByTestId("org-card-jO8BwsPe1lSCAo1gIRa6oR8vGpH3");
-  expect(orgCard).toBeDefined();
-  await fireEvent.click(orgCard);
-
-
-  await render(
-    <MemoryRouter initialEntries={['/dashboard/jO8BwsPe1lSCAo1gIRa6oR8vGpH3']}>
-      <Routes>
-        <Route path="/dashboard/:orgId" element={<OrgPage />} />
-      </Routes>
-    </MemoryRouter>);
-
-  await waitFor(() => {
-    const about = screen.getByText(/Facebook/i);
-    expect(about).toBeDefined();
-  });
+  await renderOrgPage()
 
   await waitFor(() => {
     const posts = screen.getByTestId("posts-tab");
-    fireEvent.click(posts);
+    userEvent.click(posts);
     }
   );
 
+  await timeout(3000);
+    
+  const createNewPostButton = await screen.findByTestId("create-post");
+  await expect(createNewPostButton).toBeDefined();
+  await userEvent.click(createNewPostButton);
+
+  const postTitleField = await screen.findByTestId("post-title-field");
+  await expect(postTitleField).toBeDefined();
+  await fireEvent.change(postTitleField, { target: { value: "test post from sprint two"}});
+
+  const postContentField = await screen.findByTestId("post-content-field");
+  await expect(postContentField).toBeDefined();
+  await fireEvent.change(postContentField, { target: { value: "test content"}});
+
+  // Add an image (dummy content) and upload it
+  const file = new File(['dummy content'], 'checkmark.png', { type: 'image/png' });
+  const postImageField = await screen.findByTestId("post-image-field")
+  await fireEvent.change(postImageField, { target: { files: [file] } });
+  
+  // Click the create post button
+  const submitPostButton = await screen.findByTestId("create-post-button");
+  await expect(submitPostButton).toBeDefined();
+  await userEvent.click(submitPostButton);
+
+  await timeout(5000);
+
+  // Check if the image has been uploaded alt: "Upload preview ${index + 1}""
+  await waitFor(() => {
+    const uploadedImage = screen.findAllByAltText('Upload preview 1');
+    expect(uploadedImage).toBeDefined();
+  });
+
   await new Promise((r) => setTimeout(r, 2000));
-    // Check current amount of test posts 
-    const postsCurrent = screen.queryAllByText(/This is a test post. Courtesy of SprintTwo_Post.test.tsx/i);
 
-    await fireEvent.click(screen.getByText(/Create New Post/i));
-    
-    console.log(postsCurrent.length);
-    await user.type(screen.getByPlaceholderText(/Title/i), "Test Post (SprintTwo_Post.test.tsx)");
-    await user.type(screen.getByPlaceholderText(/Content/i), "This is a test post. Courtesy of SprintTwo_Post.test.tsx");
-    
-    // Add an image (dummy content) and upload it
-    const file = new File(['dummy content'], 'checkmark.png', { type: 'image/png' });
-    const input = screen.getByLabelText(/Add Images/i);
-    fireEvent.change(input, { target: { files: [file] } });
-    
-    // Click the create post button
-    await user.click(screen.getByTestId("create-post-button"));
+  //edit post
+  await waitFor(() => {
+    const editButtons = screen.queryAllByTestId("edit-post-button");
+    userEvent.click(editButtons[0]);
 
-    // Wait for the post to be created 
-    await waitFor(() => {
-      const postsCreated = screen.queryAllByText(/This is a test post. Courtesy of SprintTwo_Post.test.tsx/i);
+    const postTitleField = screen.getByTestId("edit-post-title")
+    const postContentField = screen.getByTestId("edit-post-content");
+    const saveChangesField = screen.getByTestId("save-changes-post");
 
-      expect(postsCreated.length).toBeGreaterThan((postsCurrent.length));
-      console.log(postsCreated.length);
-    });
+    expect(postTitleField).toBeDefined();
+    expect(postContentField).toBeDefined();
+    expect(saveChangesField).toBeDefined();
 
-    // Check if the image has been uploaded alt: "Upload preview ${index + 1}""
-    await waitFor(() => {
-      const uploadedImage = screen.getByAltText('Upload preview 1');
-      expect(uploadedImage).toBeDefined();
-    });
+    fireEvent.change(postTitleField, {target: {value: "post edit name"}});
+    fireEvent.change(postContentField, {target: {value: "post edit desc"}});
+    fireEvent.click(saveChangesField);
+  })
+  // Verify that the alert was called
+  await waitFor(() => {
+    expect(spyAlert).toHaveBeenCalledWith('Post updated successfully!');
+  });
+  // Restore the original alert function
+  spyAlert.mockRestore();
 
-    await new Promise((r) => setTimeout(r, 2000));
+  await timeout(5000);
   
-    
-    await waitFor(() => {
-      const editButtons = screen.queryAllByTestId("edit-post-button");
-      user.click(editButtons[0]);
-  
-      const postTitleField = screen.getByTestId("edit-post-title")
-      const postContentField = screen.getByTestId("edit-post-content");
-      const saveChangesField = screen.getByTestId("save-changes-post");
-  
-      expect(postTitleField).toBeInTheDocument();
-      expect(postContentField).toBeInTheDocument();
-      expect(saveChangesField).toBeInTheDocument();
-  
-      fireEvent.change(postTitleField, {target: {value: "post edit name"}});
-      fireEvent.change(postContentField, {target: {value: "post edit desc"}});
-      fireEvent.click(saveChangesField);
-    })
-    // Verify that the alert was called
-    await waitFor(() => {
-      expect(spyAlert).toHaveBeenCalledWith('Post updated successfully!');
-    });
-    // Restore the original alert function
-    spyAlert.mockRestore();
-  
-    // Verify that the changes are reflected
-    await waitFor(() => {
-      expect(screen.queryAllByText("post edit name").length).toBeGreaterThan(0);
-      expect(screen.queryAllByText("post edit desc").length).toBeGreaterThan(0);
-  
-    });
+  // Verify that the changes are reflected
+  await waitFor(() => {
+    expect(screen.queryAllByText("post edit name").length).toBeGreaterThan(0);
+    expect(screen.queryAllByText("post edit desc").length).toBeGreaterThan(0);
+  });
 
-    // Delete the specific post using the first delete button found
-    await waitFor(() =>{
-      const deleteButtons = screen.queryAllByTestId("delete-post-button");
-      if (deleteButtons.length != 0) {
-        user.click(deleteButtons[0]);
-        user.click(deleteButtons[1]);
-      }
-    })
+  // Delete the specific post using the first delete button found
+  await waitFor(() =>{
+    const deleteButtons = screen.queryAllByTestId("delete-post-button");
+    if (deleteButtons.length != 0) {
+      userEvent.click(deleteButtons[0]);
+    }
+  })
 
-    await new Promise((r) => setTimeout(r, 3000));
+  await waitFor(() => {
+    expect(spyConfirm).toHaveBeenCalledWith('Are you sure you want to delete this post?');
+  });
+  // Restore the original alert function
+  spyConfirm.mockRestore();
 
-    await waitFor(() => {
-      const postsDeleted = screen.queryAllByText(/This is a test post. Courtesy of SprintTwo_Post.test.tsx/i);
-      expect(postsDeleted).toHaveLength(0);
+  await timeout(3000);
 
-    });
+  await waitFor(() => {
+    const postsDeleted = screen.queryAllByText(/test post from sprint two/i);
+    expect(postsDeleted).toHaveLength(0);
+  });
 
-    await logOut();
-  }, 50000);
+  await logOut();
+  }, 100000);
 
 
-
+/*
   test('navigate to org page from dashboard, unable to find delete post button', async  () => {
     await logInUser();
     cleanup();
@@ -207,4 +216,4 @@ test('navigate to org page from dashboard, create a post, delete a post', async 
       }
     );
     await logOut();
-  }, 15000);
+  }, 100000);*/
